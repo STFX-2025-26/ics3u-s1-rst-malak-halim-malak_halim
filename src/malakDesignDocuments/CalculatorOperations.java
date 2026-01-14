114c6462eed752ac8f8b286a87293d22a33f70f3
s506124 <s506124@FXH-6764418147>
2026-01-14 08:21:20 -0500

    m

---
+++ src/malakDesignDocuments/CalculatorOperations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+package malakDesignDocuments;
+
+public class CalculatorOperations {
+
+	public static void main(String[] args) {
+		
+		//variables
+		Scanner input = new Scanner(System.in);
+		double num1;
+		double num2;
+		int choice;
+		double result=0;
+		
+		// input
+		System.out.print("Enter first number: ");
+		num1 = input.nextDouble();
+		System.out.print("Enter second number.");
+		num2 = input.nextDo
+		
+		
+		
+
+	}
+
+}
