f9c430987b5f78e374f1ee172a8cc9a8e78e1bb6
s506124 <s506124@FXH-6764418147>
2026-01-16 08:22:41 -0500

    CalculatorOperations code is done with the test plan.

--- src/malakJavaCode/CalculatorOperations.java
+++ src/malakJavaCode/CalculatorOperations.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
-
 package malakJavaCode;
+
+import java.util.Scanner;
 
 public class CalculatorOperations {
 
@@ -12,13 +11,42 @@ public class CalculatorOperations {
 		double num1;
 		double num2;
 		int choice;
-		double result=0;
+		int result=0;
 		
-		// input
 		System.out.print("Enter first number: ");
-		num1 = input.nextDouble();
-		System.out.print("Enter second number.");
-		num2 = input.nextDo
+	    num1 = input.nextDouble();
+	    System.out.print("Enter second number: ");
+	    num2 = input.nextDouble();
+	    System.out.println("\n1 - Addition");
+	    System.out.println("2 - Subtraction");
+	    System.out.println("3 - Multiplication");
+	    System.out.println("4 - Division");
+	    System.out.print("Choose an option: ");
+	    choice = input.nextInt();
+	    if (choice == 1) {
+	        result = (int)(num1 + num2);
+	       } else if (choice == 2) {
+	           result = (int)(num1 - num2);
+	       } else if (choice == 3) {
+	           result = (int)(num1 * num2);
+	       } else if (choice == 4) {
+	          if (num2 == 0) {
+	              System.out.println("Error.");
+	              input.close();
+	              return;
+	           }
+	           result = (int)(num1 / num2);
+	       } else {
+	          System.out.println("Invalid choice.");
+	          input.close();
+	          return;
+	       }
+	       System.out.println("Result: " + result);
+	       input.close();
+
+		
+		
+		
 		
 		
 		
